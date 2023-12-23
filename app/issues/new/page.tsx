0be81def3f214490cbb216db6b6371b09fba1912
Luigi Lupini <luigi.lupini@gmail.com>
2023-12-23 21:52:13 +0200

    * page component (/issues/new/page.tsx)

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,9 @@
+import IssueForm from "./issue-form";
+
+export default function NewIssuePage() {
+  return (
+    <main className="mx-auto">
+      <IssueForm />
+    </main>
+  );
+}
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,16 @@
 import { readIssues } from "@/libs/actions/issues";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
-type Props = {};
-
-export default async function IssuesPage({}: Props) {
+export default async function IssuesPage() {
   const issues = await readIssues();
   console.log(issues);
   return (
     <main>
       <p>Issues Page</p>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
     </main>
   );
 }
